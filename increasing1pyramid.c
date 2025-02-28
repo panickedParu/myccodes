7deaa79bf80ba6f756fd9954d06426dffed594ac
panickedParu <hasanparvez583@gmail.com>
2025-02-28 21:06:21 +0600

    increasing1pyramid.c

--- increasing1pyramid.c
+++ increasing1pyramid.c
@@ -1,1 +1,11 @@
-
+#include<stdio.h>
+int main()
+{
+    int n,j,i,k=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            printf("%d",k=k+1);}
+            printf("\n");
+        }
+    }
